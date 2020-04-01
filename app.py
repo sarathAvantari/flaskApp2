f29dbe69308b460ae0621a1c75ae33da175a8b50
nettem sarath <nettem@nvestbank.com>
2020-04-01 18:22:19 +0530

    change in the Hello World

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    print("Hello Container...")
-    return "Hello World 22 CodeBUild Mar 30 17:23!"
+    print("Hello Container... Apr 1 18:21 ########")
+    return "Hello World 22 CodeBUild Apr 1 18:21!"
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int("8080"), debug=True)
