4321a07db44c9660966632d022333e13f386a5a9
nettem sarath <nettem@nvestbank.com>
2020-04-01 16:13:15 +0530

    console Hello

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
+    print("Hello Container...")
     return "Hello World 22 CodeBUild Mar 30 17:23!"
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int("8080"), debug=True)
